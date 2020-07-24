6aad1e832fc01d83097756e27989c0d855768108
Henry Johnston-Ellis <henry.johnstonellis@icloud.com>
2020-07-24 13:53:13 +0100

    Add data

---
+++ R/data.R
@@ -0,0 +1,5 @@
+#' List of environment words from Pencle and Malaescu (2015) used for text
+#' classification.
+#' 
+#' @source \url{http://search.ebscohost.com.libproxy.ucl.ac.uk/login.aspx?direct=true&AuthType=ip,shib&db=bth&AN=121029499&site=ehost-live&scope=site}
+"csr_dictionary"
